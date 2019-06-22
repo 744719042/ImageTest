90d789afe18b62000607ab0909487e817ec72c80
zhangxingzhong <744719042@qq.com>
2019-06-22 15:22:19 +0800

    添加Runtime获取内存大小

--- app/src/main/java/com/example/imagetest/MemoryTestActivity.java
+++ app/src/main/java/com/example/imagetest/MemoryTestActivity.java
@@ -2,14 +2,19 @@ package com.example.imagetest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 public class MemoryTestActivity extends AppCompatActivity {
-
+    private static final String TAG = "MemoryTestActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_memory_test);
+
+        Log.e(TAG, "Total Memory = " + Runtime.getRuntime().totalMemory());
+        Log.e(TAG, "Free Memory = " + Runtime.getRuntime().freeMemory());
+        Log.e(TAG, "Max Memory = " + Runtime.getRuntime().maxMemory());
     }
 
     public void testMemory(View view) {
