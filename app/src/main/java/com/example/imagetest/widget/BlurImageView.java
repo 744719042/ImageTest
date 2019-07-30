4b1493090f95d657b83b7b0120e55c8722026ff0
xingzhongzhang <xingzhongzhang@sohu-inc.com>
2019-07-30 11:06:37 +0800

    阴影支持圆角

--- app/src/main/java/com/example/imagetest/widget/BlurImageView.java
+++ app/src/main/java/com/example/imagetest/widget/BlurImageView.java
@@ -7,12 +7,15 @@ import android.graphics.Paint;
 import android.graphics.RadialGradient;
 import android.graphics.RectF;
 import android.graphics.Shader;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.AppCompatImageView;
 import android.util.AttributeSet;
 
-public class BlurImageView extends android.support.v7.widget.AppCompatImageView {
+public class BlurImageView extends AppCompatImageView {
     private Paint paint;
+    private Paint bgPaint;
     private int[] colors = new int[] { 0x19979CA5, 0x19979CA5, 0xffffffff };
-    private float[] positions = new float[] { 0, 0.1f, 1f };
+    private float[] positions = new float[] { 0, 0.05f, 1f };
     private int rShadowLength = 15;
     private RectF lShadowRect;
     private LinearGradient lShadowGradient;
@@ -25,6 +28,9 @@ public class BlurImageView extends android.support.v7.widget.AppCompatImageView 
     private RectF bShadowRect;
     private LinearGradient bShadowGradient;
 
+    private int lrMargin = 5;
+    private float scale = 1.3f;
+
     private RectF rbCornerRect;
     private RadialGradient rbGradient;
 
@@ -35,11 +41,11 @@ public class BlurImageView extends android.support.v7.widget.AppCompatImageView 
         this(context, null);
     }
 
-    public BlurImageView(Context context, AttributeSet attrs) {
+    public BlurImageView(Context context, @Nullable AttributeSet attrs) {
         this(context, attrs, 0);
     }
 
-    public BlurImageView(Context context, AttributeSet attrs, int defStyleAttr) {
+    public BlurImageView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         init();
     }
@@ -47,6 +53,12 @@ public class BlurImageView extends android.support.v7.widget.AppCompatImageView 
     private void init() {
         setLayerType(LAYER_TYPE_SOFTWARE, null);
         paint = new Paint();
+        paint.setDither(true);
+        paint.setAntiAlias(true);
+        bgPaint = new Paint();
+        bgPaint.setDither(true);
+        bgPaint.setAntiAlias(true);
+        bgPaint.setColor(0x19979CA5);
         lShadowRect = new RectF();
         rShadowRect = new RectF();
         bShadowRect = new RectF();
@@ -57,20 +69,20 @@ public class BlurImageView extends android.support.v7.widget.AppCompatImageView 
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
-        lShadowRect.set(0, 0, lShadowLength, getHeight() - bShadowLength);
-        lShadowGradient = new LinearGradient(lShadowLength, 0, 0, 0, colors, positions, Shader.TileMode.CLAMP);
+        lShadowRect.set(lrMargin, 0, lShadowLength, getHeight() - bShadowLength);
+        lShadowGradient = new LinearGradient(lShadowLength, 0, lrMargin, 0, colors, positions, Shader.TileMode.CLAMP);
 
-        rShadowRect.set(getWidth() - rShadowLength, 0, getWidth(), getHeight() - rShadowLength);
-        rShadowGradient = new LinearGradient(getWidth() - rShadowLength, 0, getWidth(), 0, colors, positions, Shader.TileMode.CLAMP);
+        rShadowRect.set(getWidth() - rShadowLength, 0, getWidth() - lrMargin, getHeight() - rShadowLength);
+        rShadowGradient = new LinearGradient(getWidth() - rShadowLength, 0, getWidth() - lrMargin, 0, colors, positions, Shader.TileMode.CLAMP);
 
         bShadowRect.set(lShadowLength, getHeight() - bShadowLength, getWidth() - rShadowLength, getHeight());
         bShadowGradient = new LinearGradient(0, getHeight() - bShadowLength, 0, getHeight(), colors, positions, Shader.TileMode.CLAMP);
 
-        rbCornerRect.set(getWidth() - 2 * rShadowLength, getHeight() - 2 * rShadowLength, getWidth(), getHeight());
-        rbGradient = new RadialGradient(getWidth() - rShadowLength, getHeight() - rShadowLength, rShadowLength, colors, positions, Shader.TileMode.CLAMP);
+        rbCornerRect.set(getWidth() - 2 * bShadowLength, getHeight() - 2 * bShadowLength, getWidth(), getHeight());
+        rbGradient = new RadialGradient((int) (getWidth() - scale * bShadowLength), (int) (getHeight() - scale * bShadowLength), (int) (scale * bShadowLength), colors, positions, Shader.TileMode.CLAMP);
 
-        lbCornerRect.set(0, getHeight() - 2 * lShadowLength, lShadowLength * 2, getHeight());
-        lbGradient = new RadialGradient(lShadowLength, getHeight() - lShadowLength, lShadowLength, colors, positions, Shader.TileMode.CLAMP);
+        lbCornerRect.set(0, getHeight() - 2 * bShadowLength, bShadowLength * 2, getHeight());
+        lbGradient = new RadialGradient((int) (scale * bShadowLength), (int) (getHeight() - scale * bShadowLength), (int) (scale * bShadowLength), colors, positions, Shader.TileMode.CLAMP);
     }
 
     @Override
@@ -91,7 +103,10 @@ public class BlurImageView extends android.support.v7.widget.AppCompatImageView 
         canvas.drawArc(rbCornerRect, 0, 90, true, paint);
 
         paint.setShader(lbGradient);
-        canvas.drawArc(lbCornerRect, 180, 90, true, paint);
+        canvas.drawArc(lbCornerRect, 90, 90, true, paint);
+
+//        canvas.drawRect(lShadowLength, 0, getWidth() - rShadowLength,
+//                getHeight() - bShadowLength, bgPaint);
         canvas.restore();
     }
 }
